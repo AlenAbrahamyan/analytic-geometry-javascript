cebe708cb41c06585ed5144d83fef1281f00913a
AlenAbrahamyan <alenabrahamyan0@gmail.com>
2022-08-05 20:18:23 +0400

    new_examples

---
+++ flexible-element/script.js
@@ -0,0 +1,188 @@
+var box = document.getElementById("box");
+var boxWrapper = document.getElementById("box-wrapper");
+const minWidth = 40;
+const minHeight = 40;
+
+
+var initX, initY, mousePressX, mousePressY, initW, initH, initRotate;
+
+function repositionElement(x, y) {
+    boxWrapper.style.left = x + 'px';
+    boxWrapper.style.top = y + 'px';
+}
+
+function resize(w, h) {
+    box.style.width = w + 'px';
+    box.style.height = h + 'px';
+}
+
+
+function getCurrentRotation(el) {
+    var st = window.getComputedStyle(el, null);
+    var tm = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform")
+    "none";
+    if (tm != "none") {
+        var values = tm.split('(')[1].split(')')[0].split(',');
+        var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI));
+        return (angle < 0 ? angle + 360 : angle);
+    }
+    return 0;
+}
+
+function rotateBox(deg) {
+    boxWrapper.style.transform = `rotate(${deg}deg)`;
+}
+
+// drag support
+boxWrapper.addEventListener('mousedown', function (event) {
+    if (event.target.className.indexOf("dot") > -1) {
+        return;
+    }
+
+    initX = this.offsetLeft;
+    initY = this.offsetTop;
+    mousePressX = event.clientX;
+    mousePressY = event.clientY;
+
+
+    function eventMoveHandler(event) {
+        repositionElement(initX + (event.clientX - mousePressX),
+            initY + (event.clientY - mousePressY));
+    }
+
+    boxWrapper.addEventListener('mousemove', eventMoveHandler, false);
+    window.addEventListener('mouseup', function eventEndHandler() {
+        boxWrapper.removeEventListener('mousemove', eventMoveHandler, false);
+        window.removeEventListener('mouseup', eventEndHandler);
+    }, false);
+
+}, false);
+// done drag support
+
+// handle resize
+var rightMid = document.getElementById("right-mid");
+var leftMid = document.getElementById("left-mid");
+var topMid = document.getElementById("top-mid");
+var bottomMid = document.getElementById("bottom-mid");
+
+var leftTop = document.getElementById("left-top");
+var rightTop = document.getElementById("right-top");
+var rightBottom = document.getElementById("right-bottom");
+var leftBottom = document.getElementById("left-bottom");
+
+function resizeHandler(event, left = false, top = false, xResize = false, yResize = false) {
+    initX = boxWrapper.offsetLeft;
+    initY = boxWrapper.offsetTop;
+    mousePressX = event.clientX;
+    mousePressY = event.clientY;
+
+    initW = box.offsetWidth;
+    initH = box.offsetHeight;
+
+    initRotate = getCurrentRotation(boxWrapper);
+    var initRadians = initRotate * Math.PI / 180;
+    var cosFraction = Math.cos(initRadians);
+    var sinFraction = Math.sin(initRadians);
+    function eventMoveHandler(event) {
+        var wDiff = (event.clientX - mousePressX);
+        var hDiff = (event.clientY - mousePressY);
+        var rotatedWDiff = cosFraction * wDiff + sinFraction * hDiff;
+        var rotatedHDiff = cosFraction * hDiff - sinFraction * wDiff;
+
+        var newW = initW, newH = initH, newX = initX, newY = initY;
+
+        if (xResize) {
+            if (left) {
+                newW = initW - rotatedWDiff;
+                if (newW < minWidth) {
+                  newW = minWidth;
+                  rotatedWDiff = initW - minWidth;
+                }
+            } else {
+                newW = initW + rotatedWDiff;
+                if (newW < minWidth) {
+                  newW = minWidth;
+                  rotatedWDiff = minWidth - initW;
+                }
+            }
+            newX += 0.5 * rotatedWDiff * cosFraction;
+            newY += 0.5 * rotatedWDiff * sinFraction;
+        }
+
+        if (yResize) {
+            if (top) {
+                newH = initH - rotatedHDiff;
+                if (newH < minHeight) {
+                  newH = minHeight;
+                  rotatedHDiff = initH - minHeight;
+                }
+            } else {
+                newH = initH + rotatedHDiff;
+                if (newH < minHeight) {
+                  newH = minHeight;
+                  rotatedHDiff = minHeight - initH;
+                }
+            }
+            newX -= 0.5 * rotatedHDiff * sinFraction;
+            newY += 0.5 * rotatedHDiff * cosFraction;
+        }
+
+        resize(newW, newH);
+        repositionElement(newX, newY);
+    }
+
+
+    window.addEventListener('mousemove', eventMoveHandler, false);
+    window.addEventListener('mouseup', function eventEndHandler() {
+        window.removeEventListener('mousemove', eventMoveHandler, false);
+        window.removeEventListener('mouseup', eventEndHandler);
+    }, false);
+}
+
+
+rightMid.addEventListener('mousedown', e => resizeHandler(e, false, false, true, false));
+leftMid.addEventListener('mousedown', e => resizeHandler(e, true, false, true, false));
+topMid.addEventListener('mousedown', e => resizeHandler(e, false, true, false, true));
+bottomMid.addEventListener('mousedown', e => resizeHandler(e, false, false, false, true));
+leftTop.addEventListener('mousedown', e => resizeHandler(e, true, true, true, true));
+rightTop.addEventListener('mousedown', e => resizeHandler(e, false, true, true, true));
+rightBottom.addEventListener('mousedown', e => resizeHandler(e, false, false, true, true));
+leftBottom.addEventListener('mousedown', e => resizeHandler(e, true, false, true, true));
+
+// handle rotation
+var rotate = document.getElementById("rotate");
+rotate.addEventListener('mousedown', function (event) {
+    // if (event.target.className.indexOf("dot") > -1) {
+    //     return;
+    // }
+
+    initX = this.offsetLeft;
+    initY = this.offsetTop;
+    mousePressX = event.clientX;
+    mousePressY = event.clientY;
+
+
+    var arrow = document.querySelector("#box");
+    var arrowRects = arrow.getBoundingClientRect();
+    var arrowX = arrowRects.left + arrowRects.width / 2;
+    var arrowY = arrowRects.top + arrowRects.height / 2;
+
+    function eventMoveHandler(event) {
+        var angle = Math.atan2(event.clientY - arrowY, event.clientX - arrowX) + Math.PI / 2;
+        rotateBox(angle * 180 / Math.PI);
+    }
+
+    window.addEventListener('mousemove', eventMoveHandler, false);
+
+    window.addEventListener('mouseup', function eventEndHandler() {
+        window.removeEventListener('mousemove', eventMoveHandler, false);
+        window.removeEventListener('mouseup', eventEndHandler);
+    }, false);
+}, false);
+
+resize(200, 350);
+repositionElement(300, 300);
---
+++ vse-rotation-group-elements/script.js
@@ -0,0 +1,146 @@
+const triangle = document.createElement("div")
+document.body.appendChild(triangle)
+
+const createLineElement = (x, y, length, angle, color) => {
+  var line = document.createElement("div")
+  var styles =
+    "border: 2px solid " +
+    color +
+    "; " +
+    "width: " +
+    length +
+    "px; " +
+    "height: 0px; " +
+    "-moz-transform: rotate(" +
+    angle +
+    "rad); " +
+    "-webkit-transform: rotate(" +
+    angle +
+    "rad); " +
+    "-o-transform: rotate(" +
+    angle +
+    "rad); " +
+    "-ms-transform: rotate(" +
+    angle +
+    "rad); " +
+    "position: absolute; " +
+    "top: " +
+    y +
+    "px; " +
+    "left: " +
+    x +
+    "px; "
+  line.setAttribute("style", styles)
+  return line
+}
+
+const createLine = (x1, y1, x2, y2, color = "green") => {
+  var a = x1 - x2,
+    b = y1 - y2,
+    c = Math.sqrt(a * a + b * b)
+
+  var sx = (x1 + x2) / 2,
+    sy = (y1 + y2) / 2
+
+  var x = sx - c / 2,
+    y = sy
+
+  var alpha = Math.PI - Math.atan2(-b, a)
+
+  return createLineElement(x, y, c, alpha, color)
+}
+
+const boxWrapper = document.getElementById("box-wrapper")
+const elements = document.getElementsByClassName("element")
+const initialAxisElements = []
+
+for (let element of elements) {
+  const el = getComputedStyle(element)
+  initialAxisElements.push({
+    x: parseFloat(el.left) + parseFloat(el.width) / 2,
+    y: parseFloat(el.top) + parseFloat(el.height) / 2,
+    width: parseFloat(el.width),
+    height: parseFloat(el.height),
+  })
+}
+
+const getCurrentRotation = (el) => {
+  var st = window.getComputedStyle(el, null)
+  var tm =
+    st.getPropertyValue("-webkit-transform") ||
+    st.getPropertyValue("-moz-transform") ||
+    st.getPropertyValue("-ms-transform") ||
+    st.getPropertyValue("-o-transform") ||
+    st.getPropertyValue("transform")
+  ;("none")
+  if (tm != "none") {
+    var values = tm.split("(")[1].split(")")[0].split(",")
+    var angle = Math.round(Math.atan2(values[1], values[0]) * (180 / Math.PI))
+    return angle < 0 ? angle + 360 : angle
+  }
+  return 0
+}
+
+const newCoordsByRotate = (initCoords, axis, rotate) => {
+  const R = Math.sqrt(
+    (axis.x - initCoords.x) ** 2 + (axis.y - initCoords.y) ** 2
+  )
+
+  const curDeg =
+    (Math.atan((initCoords.y - axis.y) / (initCoords.x - axis.x)) * 180) /
+    Math.PI
+
+  const d = initCoords.x - axis.x < 0 ? 180 : 0
+
+  const x = R * Math.cos(((curDeg + rotate - d) / 180) * Math.PI) + axis.x
+  const y = R * Math.sin(((curDeg + rotate - d) / 180) * Math.PI) + axis.y
+  return { x, y }
+}
+
+const rotate = document.getElementById("rotate")
+
+rotate.addEventListener(
+  "mousedown",
+  () => {
+    const arrow = document.getElementById("box-wrapper")
+    const arrowRects = arrow.getBoundingClientRect()
+    const arrowX = arrowRects.left + arrowRects.width / 2
+    const arrowY = arrowRects.top + arrowRects.height / 2
+    const axis = { x: arrowX, y: arrowY }
+
+    const eventMoveHandler = (event) => {
+      const angle =
+        Math.atan2(event.clientY - axis.y, event.clientX - axis.x) + Math.PI / 2
+      const deg = (angle * 180) / Math.PI
+      boxWrapper.style.transform = `rotate(${deg}deg)`
+
+      triangle.innerHTML = ""
+
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].style.transform = `rotate(${deg}deg)`
+
+        const newPos = newCoordsByRotate(initialAxisElements[i], axis, deg)
+        elements[i].style.left =
+          newPos.x - initialAxisElements[i].width / 2 + "px"
+        elements[i].style.top =
+          newPos.y - initialAxisElements[i].height / 2 + "px"
+        triangle.appendChild(
+          createLine(newPos.x, newPos.y, axis.x, axis.y)
+        )
+      }
+    }
+
+    window.addEventListener("mousemove", eventMoveHandler, false)
+
+    window.addEventListener(
+      "mouseup",
+      function eventEndHandler() {
+        window.removeEventListener("mousemove", eventMoveHandler, false)
+        window.removeEventListener("mouseup", eventEndHandler)
+        triangle.innerHTML = ""
+      },
+      false
+    )
+  },
+  false
+)
