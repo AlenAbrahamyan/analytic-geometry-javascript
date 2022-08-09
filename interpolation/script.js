4ffc78c52367992821c92a1e723185775678b7da
Alen Abrahamyan <46008632+AlenAbrahamyan@users.noreply.github.com>
2022-08-09 14:43:41 +0400

    Update script.js

--- interpolation/script.js
+++ interpolation/script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
-
+//
 const width = (canvas.width = window.innerWidth)
 const height = (canvas.height = window.innerHeight)
 
